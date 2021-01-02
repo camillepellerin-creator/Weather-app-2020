25aacb9c7f40bfc79ef6300dd617ec20c3083b9b
Camille Pellerin <camilledanielepellerin@gmail.com>
2021-01-02 19:08:21 +0100

    Added the search engine

--- src/script.js
+++ src/script.js
@@ -30,9 +30,21 @@ function displayTemperature(response){
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
 }
-let apiKey = "13cd951ab605d6f5e3f855c8e874add0";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function search(city){
+    let apiKey = "13cd951ab605d6f5e3f855c8e874add0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
 
-axios.get(apiUrl).then(displayTemperature);
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
